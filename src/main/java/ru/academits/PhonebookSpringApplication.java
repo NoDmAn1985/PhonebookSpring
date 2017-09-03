84529913f3fe3a7fd0f70af5e22a32f37f9031f7
NoDmAn1985 <nodman@mail.ru>
2017-09-03 23:54:37 +0700

    автооткрытие браузера

--- src/main/java/ru/academits/PhonebookSpringApplication.java
+++ src/main/java/ru/academits/PhonebookSpringApplication.java
@@ -3,10 +3,28 @@ package ru.academits;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.io.IOException;
+
 @SpringBootApplication
 public class PhonebookSpringApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(PhonebookSpringApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(PhonebookSpringApplication.class, args);
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    try {
+                        Thread.sleep(5000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    Runtime.getRuntime().exec("cmd /c start http://localhost:8080");
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }).start();
+    }
 }
